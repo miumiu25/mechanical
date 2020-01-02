aaa8006c908358a942fec58030671b7ca9ab9efb
miumiu25 <837830133@qq.com>
2020-01-02 15:43:35 +0800

    init

---
+++ admin/src/views/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container>
+  <el-header height="">
+        <!-- Header content -->
+      </el-header>
+
+    <el-container>
+          <el-aside width="200px">
+      <!-- Aside content -->
+    
+      
+    </el-aside>
+      <el-main height="">
+        <!-- Main content -->
+      </el-main>
+    </el-container>
+  </el-container>
+  
+</template>
+<script>
+export default {
+  name:"Main",
+  data(){
+    return {
+      menus:[]
+    }
+  }
+}
+</script>
